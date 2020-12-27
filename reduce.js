21baa940cb23c168b7dc239140c9be26c593eb09
sam <wax654@gmail.com>
2020-12-26 21:01:46 -0800

    Wrote extractValue.

--- reduce.js
+++ reduce.js
@@ -1,12 +1,19 @@
 /*
-Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
+Write a function called extractValue which 
+accepts an array of objects and a key and 
+returns a new array with the value of each object at the key.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+    return arr.reduce(function(acc, nextobj){
+        acc.push(nextobj[key]);
+        return acc;
+    }, []);
+}
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
