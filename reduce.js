1ad136e3bffb2b86d9fa368c0d7299ae31a40a86
sam <wax654@gmail.com>
2020-12-26 21:34:57 -0800

    Wrote partition

--- reduce.js
+++ reduce.js
@@ -51,7 +51,11 @@ function vowelCount(str) {
 }
 
 /*
-Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+Write a function called addKeyAndValue 
+which accepts an array of objects and 
+returns the array of objects passed to it 
+with each object now including the key and 
+value passed to the function.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
@@ -65,10 +69,26 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    console.log('old arr',arr,key,value);
+    arr.reduce(function(input, next){
+        next[key] = input;
+        return input;
+    },value);
+    return arr;
+    
+}
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+Write a function called partition 
+which accepts an array and a callback 
+and returns an array with two arrays inside of it. 
+The partition function should run the callback function on 
+each value in the array and if the result of the callback 
+function at that specific value is true, the value should 
+be placed in the first subarray. If the result of the callback 
+function at that specific value is false, the value should 
+be placed in the second subarray. 
 
 Examples:
     
@@ -89,4 +109,10 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+    return arr.reduce(function(acc, next){
+        if(callback(next)) acc[0].push(next);
+        else acc[1].push(next);
+        return acc;
+    },[[],[]]);
+}
